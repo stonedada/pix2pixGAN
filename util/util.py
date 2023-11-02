bb47634c1c22ae7d3f02d3b96e290a79ca6f9a1a
stone <1406700750@qq.com>
2023-11-02 15:42:25 +0800

    add function about save_tif and tensor2numpy

--- util/util.py
+++ util/util.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 from PIL import Image
 import os
+import tifffile
 
 
 def tensor2im(input_image, imtype=np.uint8):
@@ -22,6 +23,27 @@ def tensor2im(input_image, imtype=np.uint8):
         if image_numpy.shape[0] == 1:  # grayscale to RGB
             image_numpy = np.tile(image_numpy, (3, 1, 1))
         image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
+    else:  # if it is a numpy array, do nothing
+        image_numpy = input_image
+    return image_numpy.astype(imtype)
+
+
+def tensor2numpy(input_image, imtype=np.float16):
+    """"Converts a Tensor array into a numpy image array.
+
+    Parameters:
+        input_image (tensor) --  the input image tensor array
+        imtype (type)        --  the desired type of the converted numpy array
+    """
+    if not isinstance(input_image, np.ndarray):
+        if isinstance(input_image, torch.Tensor):  # get the data from a variable
+            image_tensor = input_image.data
+        else:
+            return input_image
+        image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
+        # if image_numpy.shape[0] == 1:  # grayscale to RGB
+        #     image_numpy = np.tile(image_numpy, (3, 1, 1))
+        # image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
     else:  # if it is a numpy array, do nothing
         image_numpy = input_image
     return image_numpy.astype(imtype)
@@ -62,6 +84,13 @@ def save_image(image_numpy, image_path, aspect_ratio=1.0):
     if aspect_ratio < 1.0:
         image_pil = image_pil.resize((int(h / aspect_ratio), w), Image.BICUBIC)
     image_pil.save(image_path)
+
+
+def save_tif(image_numpy, image_path):
+    if image_path:
+        tifffile.imwrite(image_path, image_numpy)
+    else:
+        print("can't save image !")
 
 
 def print_numpy(x, val=True, shp=False):
